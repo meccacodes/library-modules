de0d7f58a201b8cdcd8f3229a30617d7184dfd57
Pi <pi@pi-programs.com>
2024-10-24 09:48:38 -0600

    1 spec passing - getAttribute returns only is attr exists

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
-// write your code here to make the tests pass
+// debugger;
+
+let lordOfTheRings = {
+  title: "Lord of the Rings",
+  author: "J.R.R. Tolkien",
+  checkedOut: false,
+};
+
+const Library = function () {
+  const books = [];
+
+  const checkOutBook = function (book) {
+    if (books.includes(book)) {
+      console.log(`${book.title} is checked out`);
+      return (book.checkedOut = true);
+    }
+  };
+
+  const addBook = function (book) {
+    console.log(`${book.title} has been added to the library`);
+    return books.push(book);
+  };
+
+  return {
+    addBook: addBook,
+    checkOutBook: checkOutBook,
+  };
+};
+
+// create book constructor
+const Book = function (title, author, checkedOut) {
+  const getAttribute = function (book, attribute) {
+    console.log(book[attribute]);
+  };
+
+  const setAttribute = function (book, attribute, value) {
+    book[attribute] = value;
+  };
+
+  return {
+    title,
+    author,
+    checkedOut,
+    getAttribute: getAttribute,
+    setAttribute: setAttribute,
+  };
+};
+
+const libraryInstance = Library(); // Create library instance first
+
+libraryInstance.addBook(lordOfTheRings); // Add book to library
+libraryInstance.checkOutBook(lordOfTheRings); // Check out the book
+// libraryInstance.getAttribute(lordOfTheRings, "title"); // Get attribute of book
+
+// Try adding the attributes to your Book object: -> setAttribute and getAttribute
